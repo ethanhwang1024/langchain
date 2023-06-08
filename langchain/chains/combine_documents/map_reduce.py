e88e806c18712cbe4ab627193b5b63fc4a5635b3
ethanhwang1024 <89822193+ethanhwang1024@users.noreply.github.com>
2023-06-08 09:02:26 +0800

    Update map_reduce.py

--- langchain/chains/combine_documents/map_reduce.py
+++ langchain/chains/combine_documents/map_reduce.py
@@ -87,13 +87,13 @@ class MapReduceDocumentsChain(BaseCombineDocumentsChain):
             _output_keys = _output_keys + ["intermediate_steps"]
         return _output_keys
 
-    class Config:
-        """Configuration for this pydantic object."""
+#     class Config:
+#         """Configuration for this pydantic object."""
 
-        extra = Extra.forbid
-        arbitrary_types_allowed = True
+#         extra = Extra.forbid
+#         arbitrary_types_allowed = True
 
-    @root_validator(pre=True)
+#     @root_validator(pre=True)
     def get_return_intermediate_steps(cls, values: Dict) -> Dict:
         """For backwards compatibility."""
         if "return_map_steps" in values:
@@ -101,7 +101,7 @@ class MapReduceDocumentsChain(BaseCombineDocumentsChain):
             del values["return_map_steps"]
         return values
 
-    @root_validator(pre=True)
+#     @root_validator(pre=True)
     def get_default_document_variable_name(cls, values: Dict) -> Dict:
         """Get default document variable name, if not provided."""
         if "document_variable_name" not in values:
